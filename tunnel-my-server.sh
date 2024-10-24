b12b643ae4a54490776874c827664119f85bc64d
Seung-Goo KIM <dr.seunggoo.kim@gmail.com>
2024-10-24 14:51:47 +0200

    .

--- tunnel-my-server.sh
+++ tunnel-my-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#http://localhost:8501/
-ssh -R 80:localhost:8501 ssh.localhost.run
+if [ $# -eq 0 ]; then
+    echo 'USAGE: tunnel-my-server.sh ${PORT-NUMBER}'
+fi
+
+PORT=$1
+ssh -R 80:localhost:${PORT} ssh.localhost.run
